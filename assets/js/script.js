696738dfbfaadd6ccc7d092aa22162f18a6d27e3
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-11-10 21:27:30 +0000

    Determines if the active player has won the game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,60 @@
 const cellElements = document.querySelectorAll("[data-cell]");
-const table = document.querySelector("[data-table]")
+const table = document.querySelector("[data-table]");
+const winningTextMsg = document.querySelector('[data-victory-msg]');
+const winnerMsgElement = document.querySelector('[data-winning-msg]');
+ 
 
 let circleTurn = false;
+
+let winnerChoice = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
 const toMark = (cell, AddClass) => {
     cell.classList.add(AddClass);
 };
 
+const startGame = () => {
+    // The "startGame" function defines the hover effect to show the player the "X" or the "circle".
+    for(const cell of cellElements) {
+        cell.addEventListener("click", handleClick, {once: true})
+    }
+
+    circleTurn = false;
+
+    table.classList.add("x")
+}
+
+const finalStage = (ifDraw) => {
+    if (ifDraw) {
+        winnerMsgElement.innerText = 'Draw!'
+    } else {
+        winningTextMsg.innerText = circleTurn 
+        ? 'And the circle takes the crown!' 
+        : 'X And the circle takes the crown!';
+    }
+
+    winningTextMsg.classList.add('display-victory');
+};
+
+const findWinner = (activePlayer) => {
+    // Determines if the active player has won the game.
+    return winnerChoice.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(activePlayer);
+        });
+    });
+}
+
 const changeTurns = () => {
+    // this function define who player is gonna play
     circleTurn = !circleTurn;
 
     table.classList.remove('circle')
@@ -27,7 +74,11 @@ const handleClick = (e) => {
 
     toMark(cell, AddClass);   
 
-    // Check all the victories
+    const winner = findWinner(AddClass);
+    // This functions will check all the victories
+    if (winner) {
+        finalStage(true)
+    }
 
     // Check if there is any tie
 
@@ -39,3 +90,5 @@ const handleClick = (e) => {
 for (const cell of cellElements) {
     cell.addEventListener("click", handleClick, { once: true});
 }
+
+startGame()
